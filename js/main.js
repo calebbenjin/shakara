0cc623b7febc953c926dc097a1e37d32c0a9aa4b
calebbenjin <calebben94@gmail.com>
2021-08-15 12:25:14 +0100

    section-one

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 const openBtn = document.querySelector('.nav-btn');
 const closedBtn = document.querySelector('.nav-close');
 const sideNav = document.querySelector('nav');
+const leftToggle = document.querySelector('.toggle-section .left-toggle')
+const rightToggle = document.querySelector('.toggle-section .right-toggle')
+const leftToggleContainer = document.querySelector('.toggle-body .left-container'); 
+const rightToggleContainer = document.querySelector('.toggle-body .right-container'); 
 
 openBtn.addEventListener('click', function() {
   sideNav.classList.add('open-sidebar')
 })
 closedBtn.addEventListener('click', function() {
   sideNav.classList.remove('open-sidebar')
+})
+
+
+leftToggle.addEventListener('click', function() {
+  leftToggleContainer.classList.remove('active-body')
+  leftToggle.classList.add('active');
+
+  rightToggle.classList.remove('active')
+  rightToggleContainer.classList.add('active-body')
+})
+
+rightToggle.addEventListener('click', function() {
+  rightToggleContainer.classList.remove('active-body')
+  rightToggle.classList.add('active');
+  
+  leftToggleContainer.classList.add('active-body')
+  leftToggle.classList.remove('active')
 })
